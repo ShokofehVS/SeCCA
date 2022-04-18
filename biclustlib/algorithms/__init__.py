513889390e85f580e2be60aca4a2f86689594769
ShokofehVS <sh.vahidian@gmail.com>
2022-04-18 18:49:46 +0200

    Update file

--- biclustlib/algorithms/__init__.py
+++ biclustlib/algorithms/__init__.py
@@ -1,27 +1,15 @@
 """
-    biclustlib: A Python library of biclustering algorithms and evaluation measures.
-    Copyright (C) 2017  Victor Alexandre Padilha
+    SeCCA: A Python library of privacy-preserved biclustering algorithm (Cheng and Church) with Homomorphic Encryption
 
-    This file is part of biclustlib.
+    Copyright (C) 2022  Shokofeh VahidianSadegh
 
-    biclustlib is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This file is part of SeCCA.
 
-    biclustlib is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from .bcca import BiCorrelationClusteringAlgorithm
-from .bibit import BitPatternBiclusteringAlgorithm
 from .cca import ChengChurchAlgorithm
-from .cca import ModifiedChengChurchAlgorithm
-from .las import LargeAverageSubmatrices
-from .plaid import Plaid
-from .xmotifs import ConservedGeneExpressionMotifs
+from .SeCCA import SecuredChengChurchAlgorithm
+from .type1 import SecuredChengChurchAlgorithmType1
+from .type2 import SecuredChengChurchAlgorithmType2
+from .type3 import SecuredChengChurchAlgorithmType3
+from .type4 import SecuredChengChurchAlgorithmType4
